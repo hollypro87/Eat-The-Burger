22e982e8d2d5f97ec20128a9a1ed1588bd66e1c7
Holly Prothe <hollyprothe1@gmail.com>
2020-06-14 17:10:47 -0500

    troubleshooting errors

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 
-const mysql = require('mysql');
+var mysql = require('mysql');
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3306,
         host: 'localhost',
         user: 'root',
         password: 'password',
